358031f5910449ada96eb4f5df4c6984e6920af0
saurabhkumar998 <49653295+saurabhkumar998@users.noreply.github.com>
2020-06-16 14:17:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+var player1Name = "Player 1" ;
+var player2Name = "Player 2" ;
+
+//-----------------------PLAYER NAME CHANGER----------------------------------------------
+function editName(){
+  player1Name = prompt("Enter Player 1 Name:");
+  player2Name = prompt("Enter Player 2 Name:");
+ document.getElementById("player1").innerHTML= player1Name;
+ document.getElementById("player2").innerHTML= player2Name;
+}
+
+function dice(){
+
+
+  //----------------------FOR FIRST DICE-----------------------------------
+
+  var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+  var randomImage = "dice" + randomNumber1 + ".png";
+
+  var randomImageSource = "images/" + randomImage;
+
+  var image1 = document.querySelectorAll("img")[0];
+
+  image1.setAttribute("src", randomImageSource);
+  //-----------------------FOR SECOND DICE-------------------------------
+
+  var randomNumber2 = Math.floor(Math.random()*6) + 1 ;
+
+  var randomImage2 = "dice" + randomNumber2 +".png" ;
+
+  var randomImageSource2 = "images/" + randomImage2 ;
+
+  var image2 = document.querySelectorAll("img")[1];
+
+  image2.setAttribute("src", randomImageSource2);
+
+  //---------------COMPARING AND GIVING RESULTS-------------------------------------------
+
+  if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = (player1Name + " won 👏 .");
+  }
+  else if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw !";
+  }
+  else{
+    document.querySelector("h1").innerHTML = (player2Name + " won 👏 .");
+  }
+
+}
